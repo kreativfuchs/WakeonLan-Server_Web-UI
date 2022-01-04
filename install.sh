e287264dd1cd0e6ad91c16bf062d0ee97ebb254f
Lutz Kessler <74836085+kreativfuchs@users.noreply.github.com>
2022-01-04 18:57:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 apt-get update && apt-get upgrade -y
-apt-get install apache2 php libapache2-mod-php toilet figlet -y
+apt-get install apache2 php libapache2-mod-php -y
 rm /var/www/html/index.html
 cp config/* /var/www/html/
 apt install python -y
@@ -7,5 +7,4 @@ chmod +x /var/www/html/wake.py
 python /var/www/html/wake.py
 echo -e "ALL ALL=NOPASSWD: /var/www/html/shutdown.sh" >> /etc/sudoers
 ssh-keygen -t rsa -b 4096
-figlet Installation Done!
 cat done.txt
